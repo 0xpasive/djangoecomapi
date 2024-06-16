9b6dfe21a2c35bab0bdbf12082f995cb444a1ab8
0xpasive <118454575+0xpasive@users.noreply.github.com>
2024-06-16 20:05:29 +0545

    quantity

--- ecomapi/ecomapiapp/models.py
+++ ecomapi/ecomapiapp/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE) 
     originalprice = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='product_images')
 
